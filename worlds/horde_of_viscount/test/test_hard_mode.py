4c45c47abf69ba47b403d47d93be703e98e151af
Sean RD Higgins <sean.rd.higgins@gmail.com>
2026-07-24 20:30:44 -0400

    Adjusted hard mode to have more valuable tests.

--- worlds/horde_of_viscount/test/test_hard_mode.py
+++ worlds/horde_of_viscount/test/test_hard_mode.py
@@ -14,17 +14,25 @@ class TestHardMode(HoVTestBase):
             whip_durable_b_in_itempool = self.get_items_by_name("Whip Durable B")
             self.assertTrue(len(whip_durable_b_in_itempool) == 1)
 
-        with self.subTest("Test that the final area isn't reachable without the required items"):
+        with self.subTest("Test that the final area isn't reachable without all required items"):
             self.assertAccessDependency(
-                ["Credits Peak Chest 2"],
+                ["Credits Peak Chest 1"],
                 [[SUBWEAPON.KNIFE, SUBWEAPON.AXE, SUBWEAPON.BOMB, SUBWEAPON.WINGS, SUBWEAPON.CLEATS, SUBWEAPON.CALTROP]],
                 only_check_listed=True,
             )
 
         for item in [SUBWEAPON.KNIFE, SUBWEAPON.AXE, SUBWEAPON.BOMB, SUBWEAPON.WINGS, SUBWEAPON.CLEATS, SUBWEAPON.CALTROP]:
-            with self.subTest(f"Test that the final boss requires {item}"):
+            with self.subTest(f"Test that the final difficulty on Credits Peak requires {item}"):
                 self.assertAccessDependency(
                     ["Credits Peak Chest 2"],
                     [[item]],
                     only_check_listed=True,
                 )
+
+
+        with self.subTest(f"Test everything with nothing. No clue if this does anything useful, but it should at least not crash."):
+            self.assertAccessDependency(
+                [],
+                [[]],
+                only_check_listed=False,
+            )
